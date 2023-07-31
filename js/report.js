b09187c0180d315914443c11d8542384190e2496
罗 <1403829685@qq.com>
2023-07-31 09:04:32 +0800

    report报告完成结束

--- js/report.js
+++ js/report.js
@@ -1,70 +1,143 @@
-window.onload = function () {
-    axios({
-        method: 'get',
-        url: 'http://127.0.0.1:3000/report/new',
+
+var groud=0     //组数
+var xin=document.querySelector('#new') // 最新按钮
+var hot=document.querySelector('#hot')// 最热按钮
+var xin1=document.querySelector('.new')  //新面板
+var hot1=document.querySelector('.hot')  //最热面板
+var newLod=document.querySelector('#new-lod') //新加载
+var newLoding=document.querySelector('#new-loding') //新加载面板
+var hotLod=document.querySelector('#hot-lod')   //热加载
+var hotLoding=document.querySelector('#hot-loding')   //热加载面板
+var ula=document.querySelector('#ul-hot')//热ul
+var ul=document.querySelector('#ul-new')//新ul
+
+xin.addEventListener('click',function(){
+    xin.classList.add('on')
+    hot.classList.remove('on')
+    xin1.style.display='block'
+    hot1.style.display='none'
+    groud=0  
+    ula.innerHTML=''
+    showNew()
+    
+})
+hot.addEventListener('click',function(){
+    hot.classList.add('on')
+    xin.classList.remove('on')
+    hot1.style.display='block'
+    xin1.style.display='none'
+    groud=0  
+    ul.innerHTML=''
+    showhot()
+})
+
+// 获取最新的图片地址
+newLod.addEventListener('click',function(){
+    newLod.style.display='none';
+    newLoding.style.display='block';
+    setTimeout(() => {
+        newLoding.style.display='none';
+        newLod.style.display='block';
+        showNew()
+    }, 1000);
+})
+// 
+function showNew(){
+    axios.get('http://127.0.0.1:3000/report/new').then(res=>{
+        show(res.data)
     })
-        .then(function (res) {
-            var navlist = res.data;
-            // console.log('--------');
-            // console.log(navlist);
-            var nav = document.querySelector('.bull');
-
-            // console.log(nav);
-            var lis = document.createElement('li');
-            nav.appendChild(lis);
-            var str = '';
-            navlist.forEach(function (item) {
-                // console.log(item.txt); 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                str += `<li>
-                        <a href="">
-                            <div class="img">
-                                <div class="mb"></div>
-                                <img src="${item.img}" alt="">
-                                <div class="content">
-                                    <p>${item.text}</p>
-                                    <div class="leftSpan">
-                                        <span></span>
-                                        <!-- <p>苏苏</p> -->
-                                        <span>${item.uName}</span>
-                                        <span>${item.endTime}</span>
+    
+    function show(data){
+        
+        var str='';
+        for(var i=0;i<4;i++){
+            
+            // console.log(groud);
+            str+=`
+            <li>
+                    <a href="#">
+                        <img src="${data[groud].img}" alt="">
+                        <div class="info">
+                            <p>${data[groud].text}</p>
+                            <div class="info-in">
+                                <div class="left">
+                                    <img src="./img/tx.jpg" alt="">
+                                    <span class="red">${data[groud].uName}</span>
+                                    <span class="time">${data[groud].endTime}</span>
+                                </div>
+                                <div class="right">
+                                    <span class="zan">3</span>
+                                    <span class="xinxi">3</span>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                    <p class="look-about">关于格林威特空气净化器还有 4 篇报告，点击查看</p>
+                </li>
+            `       
+            groud++
+            
+            }
+        ul.innerHTML+=str
+        if(groud>=data.length-1){
+            newLod.style.display='none';
+            newLoding.style.display='none';
+        }
+    }
+}
+// 获取最热的图片地址
+hotLod.addEventListener('click',function(){
+    hotLod.style.display='none';
+    hotLoding.style.display='block';
+    setTimeout(() => {
+        hotLoding.style.display='none';
+        hotLod.style.display='block';
+        showhot()
+    }, 1000);
+})
+function showhot(){
+    axios.get('http://127.0.0.1:3000/report/hot').then(res=>{
+        show1(res.data)
+        console.log(res.data);
+    })
+    
+    function show1(data){
+        var str='';
+        
+            for(var i=0;i<4;i++){
+                if(groud<=data.length-1){
+                console.log(groud);
+                str+=`
+                <li>
+                        <a href="#">
+                            <img src="${data[groud].img}" alt="">
+                            <div class="info">
+                                <p>${data[groud].text}</p>
+                                <div class="info-in">
+                                    <div class="left">
+                                        <img src="./img/tx.jpg" alt="">
+                                        <span class="red">${data[groud].uName}</span>
+                                        <span class="time">${data[groud].endTime}</span>
                                     </div>
-                                    <div class="rightSpan">
-                                        <span>3</span>
-                                        <span>3</span>
+                                    <div class="right">
+                                        <span class="zan">3</span>
+                                        <span class="xinxi">3</span>
                                     </div>
                                 </div>
                             </div>
-                            <div class="lecture">
-                                <p>关于格林威特空气净化器还有 4 篇报告，点击查看</p>
-                            </div>
                         </a>
-                            </li>`
-
-
-
-
-            });
-            nav.innerHTML = str;
-        })
-        .catch(function (error) {
-            console.log('请求发生错误:', error);
-        });
-
-
-
+                        <p class="look-about">关于格林威特空气净化器还有 4 篇报告，点击查看</p>
+                    </li>
+                `   
+                groud++
+            } else{
+                hotLod.style.display='none';
+                hotLoding.style.display='none';
+            }
+                }
+            ula.innerHTML+=str
+    }
 }
 
+// 初始化页面
+showNew()
